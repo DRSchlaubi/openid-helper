d93ed4fe585e5b1406eb201b4cbc067ab0b146b1
Michael Rittmeister <michael@rittmeister.in>
2024-08-15 17:35:15 +0200

    Fix Bitbucket

--- src/providers/implementations/BitBucket.kt
+++ src/providers/implementations/BitBucket.kt
@@ -2,6 +2,7 @@ package dev.schlaubi.openid.helper.providers.implementations
 
 import dev.schlaubi.openid.helper.providers.ProviderRegistry
 import dev.schlaubi.openid.helper.providers.registerProvider
+import dev.schlaubi.openid.helper.util.fixLowercaseBearer
 
 fun ProviderRegistry.bitbucket() = registerProvider("bitbucket") {
     authorize("https://bitbucket.org/site/oauth2/authorize")
@@ -10,6 +11,7 @@ fun ProviderRegistry.bitbucket() = registerProvider("bitbucket") {
     userEndpoint {
         request {
             url("https://api.bitbucket.org/2.0/user")
+            fixLowercaseBearer()
         }
 
         response {
