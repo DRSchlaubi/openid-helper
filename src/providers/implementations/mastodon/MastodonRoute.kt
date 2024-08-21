b69d4523d8ca492908c09976bf142ca642b40c90
schlaubi <michael@rittmeister.in>
2024-08-22 00:03:15 +0200

    Improve design of Markdown selection page

--- src/providers/implementations/mastodon/MastodonRoute.kt
+++ src/providers/implementations/mastodon/MastodonRoute.kt
@@ -13,12 +13,13 @@ import io.ktor.http.path
 import io.ktor.http.takeFrom
 import io.ktor.server.application.call
 import io.ktor.server.html.respondHtml
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.request.receiveParameters
 import io.ktor.server.resources.get
 import io.ktor.server.resources.href
 import io.ktor.server.resources.post
-import io.ktor.server.response.respondRedirect
+import io.ktor.server.response.*
 import io.ktor.server.routing.Route
 import kotlinx.html.ButtonType
 import kotlinx.html.FormMethod
@@ -39,69 +40,12 @@ import kotlinx.html.style
 import kotlinx.html.title
 import kotlinx.html.unsafe
 import org.intellij.lang.annotations.Language
+import java.io.File
+import java.net.URI
 import kotlin.text.startsWith
 
 
 private val LOG = KotlinLogging.logger { }
-
-@Language("CSS")
-private val css = """
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-body {
-    background-color: black;
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-}
-
-input {
-    background-color: inherit; /* Uses the normal background color of its parent */
-    border-radius: 8px; /* Adjust the radius as needed for desired rounded effect */
-    padding: 10px; /* Optional: Adds some padding for better appearance */
-    border: 1px solid #ccc; /* Optional: Adds a border color */
-}
-form input {
-    display: block;
-    margin-bottom: 10px; /* Adds space between inputs */
-    width: 100%; /* Makes inputs take full width of the container */
-    box-sizing: border-box; /* Ensures padding and border are included in the element's total width and height */
-}
-
-.centered-div {
-    position: fixed;
-    color: white;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: rgb(43, 45, 49);
-    border-radius: 15px;
-    padding: 20px;
-    text-align: center;
-}
-.error {
-    color: red
-}
-
-@media screen and (max-width: 600px) {
-    .centered-div {
-        width: 100% !important;
-        height: 100% !important;
-        top: 0 !important;
-        left: 0 !important;
-        transform: none !important;
-        border-radius: 0;
-    }
-}    
-/* Style for the button to differentiate interactively */
-.centered-div form button {
-    background-color: darkgray;
-    color: white; /* Text color for the button */
-    cursor: pointer; /* Changes cursor to pointer on hover */
-    border-radius: 8px; /* More rounded corners */
-    padding: 8px;
-    border: none; /* Removes the default border */
-}
-
-""".trimIndent()
 
 private data class State(val app: MastodonOAuthApplication, val redirect: String, val url: String)
 
@@ -111,57 +55,9 @@ fun Route.mastodon() {
     get<Mastodon.SelectHost> { route ->
         call.verifyOauth()
         val (_, _, _, _, _, isInvalid, prefill) = route
-        call.respondHtml {
-            head {
-                title = "Mastodon selector"
-                meta("viewport", "width=device-width, initial-scale=1.0")
-            }
 
-            body {
-                style {
-                    unsafe {
-                        +css
-                    }
-                }
-
-                div("centered-div") {
-                    img(
-                        alt = "Mastodon logo",
-                        src = "https://files.mastodon.social/accounts/avatars/000/013/179/original/b4ceb19c9c54ec7e.png"
-                    ) {
-                        height = "128"
-                        width = "128"
-                    }
-                    h2 { +"Please enter the Mastodon host below" }
-                    if (isInvalid) {
-                        p("error") {
-                            id = "error-message"
-                            +"This host is invalid"
-                        }
-                    }
-                    form(method = FormMethod.post) {
-                        input(InputType.text, name = "host") {
-                            required = true
-                            placeholder = "mastodon.social"
-                            //language=JavaScript
-                            onChange = """
-                                document.getElementById("submit-button").disabled = false
-                                document.getElementById("error-message").remove()
-                            """.trimIndent()
-                            if (prefill != null) {
-                                value = prefill
-                            }
-                        }
-
-                        button(type = ButtonType.submit) {
-                            id = "submit-button"
-                            disabled = isInvalid
-                            +"Submit"
-                        }
-                    }
-                }
-            }
-        }
+        val file = ClassLoader.getSystemResource("mastodon/select-host.html")
+        call.respondFile(File(file.toURI()))
     }
 
     post<Mastodon.SelectHost> {
